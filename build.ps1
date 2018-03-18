ed83fbf2bf10b313ccdedf0519bdcf9893664afe
Cole Campbell <cole.campbell@twistedlogik.net>
2018-03-18 17:15:28 -0400

    Add restore step to build script

--- build.ps1
+++ build.ps1
@@ -2,5 +2,7 @@ $command = "cmd.exe /C cargo build --manifest-path projects/ironcore-native/Carg
 Invoke-Expression -Command:$command
 $command = "cmd.exe /C cargo build --manifest-path projects/ironcore-host/Cargo.toml"
 Invoke-Expression -Command:$command
+$command = "cmd.exe /C msbuild /t:Restore projects/ironcore-managed.sln"
+Invoke-Expression -Command:$command
 $command = "cmd.exe /C msbuild projects/ironcore-managed.sln"
 Invoke-Expression -Command:$command
